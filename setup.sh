586b09b5d3a3e9d77df16b833e30231667f7268f
Ricardo Magana <magania@gmail.com>
2017-04-10 17:40:41 +0000

    change tensorflow to GPU version

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 CUDA_DOWNLOAD_LINK=https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
-TENSORFLOW_WHEEL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.1.0rc1-cp34-cp34m-linux_x86_64.whl
+TENSORFLOW_WHEEL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.0.1-cp34-cp34m-linux_x86_64.whl
 
 # echo ${SSH_PUB_KEY} >> .ssh/authorized_keys
 
@@ -16,5 +16,6 @@ cd /usr/local/ && sudo tar xzf ~/${CUDNN_TGZ} && cd
 sudo pip3 install --upgrade -r requirements.txt
 sudo pip3 install --upgrade ${TENSORFLOW_WHEEL}
 
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8888
 
+echo "Done. Run jupyter notebook --no-browser --ip=0.0.0.0"
